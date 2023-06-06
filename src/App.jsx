1dcaa2f917c2cc950d9ea1684e535a7b9754d159
Plangfromwow <jacobwantswow@gmail.com>
2023-06-05 23:10:07 -0400

    init commit with tailwind and some changes to its config file

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-3xl font-bold underline">
+      <h1>This is the background now </h1>
+    </div>
+  );
+}
+
+export default App;
