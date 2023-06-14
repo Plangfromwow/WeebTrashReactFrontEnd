974f1b3ca49e6408a7f201352f1d24c15a51900b
Plangfromwow <jacobwantswow@gmail.com>
2023-06-13 21:55:32 -0400

    Added footer and some other things to the hero and nav as well as added the instagram link to the footer

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,25 +2,15 @@ export default function NavBar() {
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+        <a className="btn btn-ghost normal-case text-xl">WeebTrash</a>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <a>Link</a>
+            <a>Shop</a>
           </li>
           <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2 bg-base-100">
-                <li>
-                  <a>Link 1</a>
-                </li>
-                <li>
-                  <a>Link 2</a>
-                </li>
-              </ul>
-            </details>
+            <a>Contact Us</a>
           </li>
         </ul>
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import NavBar from "./Components/NavBar.jsx";
+import Footer from "./Components/Footer.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <NavBar />
     <App />
+    <Footer />
   </React.StrictMode>
 );
