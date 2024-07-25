89484c8737df2e9f2e3d966120990b477c6c8e7e
Plangfromwow <jacobwantswow@gmail.com>
2024-07-24 20:07:11 -0400

    updated events

--- src/eventsData.js
+++ src/eventsData.js
@@ -5,29 +5,7 @@
 const eventData = [
     //Should be able to copy paste one after you make it and just fill it out 
     {
-        "id": 1, //Increment this by 1 for each event
-        "name": "Spring Fling @ The 8th Side",
-        "date": "05/11/24",
-        "place": "The 8th Side",
-        "address": "4232 Fort St, Lincoln Park, MI",
-        "linkToTickets": "https://fb.me/e/23thUhO5B",
-        "description": "Local vendors will have a variety of handmade and custom items for sale including: jewelry, keychains, stickers, shirts, hats, crocheted plushies, tea, incense, anime figures, 3D printed figures, cupcakes, cookies, breads, lemonade, tacos, artwork, solar lights and more!",
-        "pic": "..\\pics\\MonroePopFestSplash.jpg"
-    },
-
-    {
-        "id": 2,
-        "name": "Otaku Detroit Summer Bash 2024!",
-        "date": "07/13/24 - 07/14/24",
-        "place": "Sheraton Detroit Novi Hotel",
-        "address": "21111 Haggerty Rd, Novi, MI",
-        "linkToTickets": "https://www.otakudetroit.com/registration-info",
-        "description": "It will be 2 days of jammed packed fun including gaming, panels, Saturday night Rave, Flea Market, Cosplay Masquerade and much more! More information will be coming very soon, including a special room rate and badge info.",
-        "pic": "..\\pics\\OTAKUDETROIT.jpg"
-    },
-
-    {
-        "id": 3,
+        "id": 1,
         "name": "Really Cool Comic Con",
         "date": "08/03/24 - 08/04/24",
         "place": "Dort Financial Center",
@@ -38,7 +16,7 @@ const eventData = [
     },
 
     {
-        "id": 4,
+        "id": 2,
         "name": "Monroe Comic Con",
         "date": "09/13/24 - 09/14/24",
         "place": "MBT Expo Center",
@@ -48,7 +26,7 @@ const eventData = [
         "pic": "..\\pics\\MONROECOMICCON.jpg"
     },
     {
-        "id": 5,
+        "id": 3,
         "name": "Youmacon",
         "date": "10/31/24 - 11/03/24",
         "place": "Huntington Place",
@@ -56,6 +34,16 @@ const eventData = [
         "linkToTickets": "https://www.youmacon.com/",
         "description": "Youmacon is a celebration of Japanese animation, videogames, popular culture and costuming, and its influence on our own culture over the past few decades. Common themes throughout the event are Anime (Japanese animation), video games, Japanese-style artwork and comics, and the rising Internet culture influenced by all of the above."
         , "pic": "..\\pics\\Youmacon.jpg"
+    },
+    {
+        "id": 4,
+        "name": "Otaku U",
+        "date": "12/07/24",
+        "place": "Schoolcraft College",
+        "address": "18600 Haggerty Rd, Livonia, MI 48152",
+        "linkToTickets": "https://otakuu.org/",
+        "description": "The meaning of Otaku to the club’s Leadership/Consulting Team and their members is being a passionate fan of anime, manga, comics, cartoons, video games, or K-Pop/J-Pop. U is taken from the word university because the convention is being held on our college campus. Our main goal is to create an event for people to come together from an array of fandoms to celebrate and share their enthusiasm with other anime fans, no matter their ages."
+        , "pic": "..\\pics\\otakuU.png"
     },
 
 
